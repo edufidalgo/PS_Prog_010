e76f755bc832c983dd19d1b3810e957dc707f47a
edufidalgo <62452382+edufidalgo@users.noreply.github.com>
2022-03-23 15:02:55 -0300

    Add files via upload

---
+++ Facil2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Facil_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($" Numero de 1's: {ContaUns(Console.ReadLine())}");
+
+            static int ContaUns(string numero)
+            {
+                int value = Int32.Parse(numero);
+                string binary = Convert.ToString(value, 2);
+                
+                int count = 0;
+                foreach (char c in binary)
+                    if (c == '1') count++;
+                return count;
+            }
+        }
+    }
+}
