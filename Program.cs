36375a1a00728b2b7b5cd1395b5cd470271aa272
edufidalgo <62452382+edufidalgo@users.noreply.github.com>
2022-03-23 19:26:35 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o número o radiano a ser convertido e arredondado:");
+
+            Console.WriteLine($"O Ângulo em graus é: {RadToDeg(double.Parse(Console.ReadLine()))}");
+            Console.Read();
+            static double RadToDeg(double rad)
+            {
+                double answer = Math.Round((double)(rad * 180/3.1415), 1);
+                return answer;
+
+            }
+        }
+    }
+}
