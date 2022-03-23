5b0917173d899c3ffc98ccb20aa50543702c2287
edufidalgo <62452382+edufidalgo@users.noreply.github.com>
2022-03-23 15:03:42 -0300

    Add files via upload

---
+++ Med2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Med_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(crossBots(Int32.Parse(Console.ReadLine())));
+
+            static string crossBots(int numero)
+            {
+                if (numero % 15 == 0)
+                    return "CrossBots";
+                else if (numero % 5 == 0)
+                    return "Bots";
+                else if (numero % 3 == 0)
+                    return "Cross";
+                else
+                    return numero.ToString();
+            }
+        }
+    }
+}
