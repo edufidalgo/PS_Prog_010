1f179bc733086a6c6f8b37d4bf9653ace566d18d
edufidalgo <62452382+edufidalgo@users.noreply.github.com>
2022-03-23 14:58:14 -0300

    Add files via upload

---
+++ Ex2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira as horas: ");
+            converter(UInt32.Parse(Console.ReadLine()));
+            Console.Read();
+            static void converter(uint horas)
+            {
+                List<uint> lista = new List<uint>();
+                if (horas >= 720)
+                    lista.Add(horas / 720);
+                else lista.Add(0);
+                if (horas >= 168)
+                    lista.Add(horas / 168);
+                else lista.Add(0);
+                lista.Add(horas * 60);
+                lista.Add(horas * 60 * 60);
+                lista.Add(horas * 60 * 60 * 1000);
+
+                string[] unidade = {"MESES" ,"SEMANAS","MINUTOS","SEGUNDOS","MILISEGUNDOS"};
+
+                int counter = 0;
+                foreach (uint elemento in lista)
+                {
+                    Console.WriteLine($"{unidade[counter]} : {elemento}");
+                    counter++;
+                }
+            }
+        }
+    }
+}
