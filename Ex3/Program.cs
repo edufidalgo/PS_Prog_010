14a5377b07a3c9e4feff4777a35c7a5e1d4a6ba1
edufidalgo <62452382+edufidalgo@users.noreply.github.com>
2022-03-23 14:58:58 -0300

    Add files via upload

---
+++ Ex3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExFacil1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva o item a ser spammado, seguido do número do spam");
+            string item = Console.ReadLine();
+            int vezes = Int32.Parse(Console.ReadLine());
+
+            int j = 1;
+            foreach (string palavra in repeat(item, vezes))
+            {
+                Console.WriteLine($"{j}:{palavra}");
+                j++;
+            }
+                                    
+
+            static string[] repeat(string item, int vezes)
+            {
+                string[] lista = new string[vezes];
+                for (int i = 0; i < vezes; i++)
+                    lista[i] = item;
+                return lista;
+            }
+        }
+    }
+}
